1b1421b865d0559eff92d7577123f7eadb4963e4
김민수 <minsu980107@naver.com>
2023-04-18 20:38:34 +0900

    코딩애플 React 기초 3강보는중

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,19 +1,24 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
-  let posts = '강남 고기 맛집';
+  let [a,b] = useState('남자 코드 추천'); 
 
+  let posts = '강남 고기 맛집';
   return (
     <div className="App">
       <div className="black-nav">
         {/* font-size -> fontSize  camelCase로 변환됨 */}
-        <div style={ {color : 'blue', fontSize : '30px'} }>개발 Blog</div>
-        {/* <div>개발 Blog</div> */}
+        {/* <div style={ {color : 'blue', fontSize : '30px'} }>개발 Blog</div> */}
+        <div>개발 Blog</div>
       </div>
-      <img src={ logo }/>
-      <h4> { posts }</h4>
+      <div className='list'>
+        <h3> { posts }</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
     </div>
   );
 }
